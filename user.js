042035c7b565d159fdb1ba86ab57e764a860bdc9
lillo84 <lillolillo84@gmail.com>
2017-08-25 16:26:58 +0200

    Add files via upload

--- user.js
+++ user.js
@@ -1,144 +1,240 @@
 var path = document.getElementById("giocatore").dataset.user;
 
 //lettura
-var giocatore = document.getElementById("giocatore");
-var dbRef = firebase.database().ref().child(path + "giocatore");
+function caricaElemento(k) {
+  var carico = document.getElementById(k);
+  var dbRef = firebase.database().ref().child(path + k);
 
-dbRef.on("value", snap => (giocatore.value = snap.val()));
-
-//abilità
-
-/* var abilita0 = document.getElementById("abilita" + 0);
-var dbRefAbi0 = firebase.database().ref().child(path + "abilita" + 0);
-dbRefAbi0.on("value", snap => (abilita0.value = snap.val()));
-
-var abilita1 = document.getElementById("abilita" + 1);
-var dbRefAbi1 = firebase.database().ref().child(path + "abilita" + 1);
-dbRefAbi1.on("value", snap => (abilita1.value = snap.val()));
-
-var abilita3 = document.getElementById("abilita" + 3);
-var dbRefAbi3 = firebase.database().ref().child(path + "abilita" + 3);
-dbRefAbi3.on("value", snap => (abilita3.value = snap.val()));
-
-var abilita4 = document.getElementById("abilita" + 4);
-var dbRefAbi4 = firebase.database().ref().child(path + "abilita" + 4);
-dbRefAbi4.on("value", snap => (abilita4.value = snap.val()));
-
-var abilita5 = document.getElementById("abilita" + 5);
-var dbRefAbi5 = firebase.database().ref().child(path + "abilita" + 5);
-dbRefAbi5.on("value", snap => (abilita5.value = snap.val())); */
-
-//variabili abilità
-function caricaAbilita(i) {
-  firebase
-    .database()
-    .ref()
-    .child(path + "abilita" + i)
-    .once("value", function(snapshot) {
-      var snapAbi = snapshot.val();
-      if (snapAbi) {
-        $("<form />", { class: "abilita", onsubmit: "return false;" })
-        .append($("<input />", { placeholder: "Abilita", class:"abi" , id: "abilita" + i }))
-        .append(
-          $("<button />", {
-            id: "submitBtn",
-            onClick: "submitClick('abilita" + i + "')"
-          })
-        )
-        .appendTo("#listaAbilita");
-      $("<form />", { class: "abilitaBonus", onsubmit: "return false;" })
-        .append($("<input />", { placeholder: "0", class:"abiBonus" , id: "abilitaBonus" + i }))
-        .append(
-          $("<button />", {
-            id: "submitBtn",
-            onClick: "submitClick('abilita" + i + "')"
-          })
-        )
-        .appendTo("#listaAbilita");
-
-        var abilita = document.getElementById("abilita" + i);
-        var dbRefAbi = firebase.database().ref().child(path + "abilita" + i);
-        dbRefAbi.on("value", snap => (abilita.value = snap.val()));
-      }
-    });
+  dbRef.on("value", snap => (carico.value = snap.val()));
 }
-caricaAbilita(0);
-caricaAbilita(1);
-caricaAbilita(2);
-caricaAbilita(3);
-caricaAbilita(4);
-caricaAbilita(5);
-caricaAbilita(6);
-caricaAbilita(7);
-caricaAbilita(8);
-caricaAbilita(9);
-caricaAbilita(10);
+
+caricaElemento("giocatore");
+caricaElemento("forza");
+caricaElemento("destrezza");
+caricaElemento("costituzione");
+caricaElemento("intelligenza");
+caricaElemento("saggezza");
+caricaElemento("carisma");
+caricaElemento("iniziativa");
+caricaElemento("Bonusinit");
+caricaElemento("puntiFerita");
+caricaElemento("puntiFeritaTot");
+caricaElemento("BAB");
+caricaElemento("BMC");
+caricaElemento("AC");
+caricaElemento("DMC");
+
 
 //scrittura
-
 function submitClick(v) {
   var mainText = document.getElementById(v);
-  /*var mainText = document.getElementById('iniziativa');*/
-  var submitBtn = document.getElementById("submitBtn");
   var firebaseRef = firebase.database().ref();
   var messageText = mainText.value;
 
   firebaseRef.child(path + v).set(messageText);
 }
 
-function genAbilita() {
-  //$('div.x').attr('id', 'y');
-  
-  for (var j = 0; j <= 10; j++) {
-    //inserire codice di controllo sull'id abilità e associarlo alla variabile i
-    
-    var str = document.getElementsByClassName("abi")[j].id;
-    if (str.indexOf(j) == -1) {
-        var i = j;
-      $("<form />", { class: "abilita", onsubmit: "return false;" })
-        .append($("<input />", { placeholder: "Abilita", class:"abi" , id: "abilita" + i }))
+function submitFocus(v) {
+  $("#" + v).focusout(function() {
+    submitClick(v);
+  });
+}
+
+submitFocus("giocatore");
+submitFocus("forza");
+submitFocus("destrezza");
+submitFocus("costituzione");
+submitFocus("intelligenza");
+submitFocus("saggezza");
+submitFocus("carisma");
+submitFocus("iniziativa");
+submitFocus("Bonusinit");
+submitFocus("puntiFerita");
+submitFocus("puntiFeritaTot");
+submitFocus("BAB");
+submitFocus("BMC");
+submitFocus("AC");
+submitFocus("DMC");
+
+//abilità
+
+
+
+//lettura delle Liste
+function caricaLista(i, k, u) {
+  firebase
+    .database()
+    .ref()
+    .child(path + k + "/" + k + i)
+    .once("value", function(snapshot) {
+      var snapAbi = snapshot.val();
+      if (snapAbi) {
+        $("<form />", { class: k, onsubmit: "return false;" })
+        .append(
+          $("<input />", {
+            placeholder: k,
+            class: u,
+            id: k + i
+          })
+        )
         .append(
           $("<button />", {
             id: "submitBtn",
-            onClick: "submitClick('abilita" + i + "')"
+            onClick: "submitListClick('" + k + i + "' , '"+ k +"')"
           })
         )
-        .appendTo("#listaAbilita");
-      $("<form />", { class: "abilitaBonus", onsubmit: "return false;" })
-        .append($("<input />", { placeholder: "0", class:"abiBonus" , id: "abilitaBonus" + i }))
+        .appendTo("#lista" + k);
+
+      $("<form />", { class: k + "Bonus", onsubmit: "return false;" })
+        .append(
+          $("<input />", {
+            placeholder: "0",
+            class: u + "Bonus",
+            id: k + "Bonus" + i
+          })
+        )
         .append(
           $("<button />", {
             id: "submitBtn",
-            onClick: "submitClick('abilita" + i + "')"
+            onClick: "submitListClick('" + k + i + "' , '"+ k +"')"
           })
         )
-        .appendTo("#listaAbilita");
-        
-        break;
-      /* var abilita2 = document.getElementById("abilita" + 2);
-      var dbRefAbi2 = firebase.database().ref().child(path + "abilita" + 2);
-      dbRefAbi2.on("value", snap => (abilita2.value = snap.val())); */
-    }
+        .appendTo("#lista" + k);
+
+        var abilita = document.getElementById(k + i);
+        var dbRefAbi = firebase.database().ref().child(path + k + "/" + k + i);
+        dbRefAbi.on("value", snap => (abilita.value = snap.val()));
+      }
+    });
+}
+caricaLista(0, "abilita", "abi");
+caricaLista(1, "abilita", "abi");
+caricaLista(2, "abilita", "abi");
+caricaLista(3, "abilita", "abi");
+caricaLista(4, "abilita", "abi");
+caricaLista(5, "abilita", "abi");
+caricaLista(6, "abilita", "abi");
+caricaLista(7, "abilita", "abi");
+caricaLista(8, "abilita", "abi");
+caricaLista(9, "abilita", "abi");
+caricaLista(10, "abilita", "abi");
+
+caricaLista(0, "inventario", "inv");
+caricaLista(1, "inventario", "inv");
+caricaLista(2, "inventario", "inv");
+caricaLista(3, "inventario", "inv");
+caricaLista(4, "inventario", "inv");
+caricaLista(5, "inventario", "inv");
+caricaLista(6, "inventario", "inv");
+caricaLista(7, "inventario", "inv");
+caricaLista(8, "inventario", "inv");
+caricaLista(9, "inventario", "inv");
+caricaLista(10, "inventario", "inv");
+
+//Genera celle Lista
+function listaNodi(i) {
+  var x = [];
+  x = $(i)
+    .map(function() {
+      return this.id;
+    })
+    .get();
+
+  x.sort();
+  var z = x.length;
+
+  for (var i = 0; i < 10 - z; i++) {
+    x.push(" ");
   }
 
-  /* var form = document.createElement("form");
-  form.className = "abilita";
-  var t = document.createElement(
-    "input"
-  ); 
-  var s = document.createElement("button");
-  s.id = "submitBtn";
-  form.appendChild(t);
-  form.appendChild(s);
-  document.getElementById("listaAbilita").appendChild(form);
-  var form2 = document.createElement("form");
-  form2.className = "abilitaBonus";
-  var t2 = document.createElement(
-    "input"
-  ); 
-  var s2 = document.createElement("button");
-  s2.id = "submitBtn";
-  form2.appendChild(t2);
-  form2.appendChild(s2);
-  document.getElementById("listaAbilita").appendChild(form2);*/
+  return x;
 }
+
+function genCelle(u, k) {
+  var ability = listaNodi("." + u + "");
+  var i = ability.length;
+
+  for (var j = 0; j <= 10; j++) {
+    if (ability[j].indexOf(j) == -1) {
+      i = j;
+
+      $("<form />", { class: k, onsubmit: "return false;" })
+        .append(
+          $("<input />", {
+            placeholder: k,
+            class: u,
+            id: k + i
+          })
+        )
+        .append(
+          $("<button />", {
+            id: "submitBtn",
+            onClick: "submitListClick('" + k + i + "' , '"+ k +"')"
+          })
+        )
+        .appendTo("#lista" + k);
+
+      $("<form />", { class: k + "Bonus", onsubmit: "return false;" })
+        .append(
+          $("<input />", {
+            placeholder: "0",
+            class: u + "Bonus",
+            id: k + "Bonus" + i
+          })
+        )
+        .append(
+          $("<button />", {
+            id: "submitBtn",
+            onClick: "submitListClick('" + k + i + "' , '"+ k +"')"
+          })
+        )
+        .appendTo("#lista" + k);
+
+      break;
+    }
+  }
+}
+//scrittura liste
+function submitListClick(g, v) {
+  var mainText = document.getElementById(g);
+  var firebaseRef = firebase.database().ref();
+  var messageText = mainText.value;
+
+  firebaseRef.child(path + v+"/"+g).set(messageText);
+}
+
+//lettura
+function caricaElementoLista(g, k) {
+  var carico = document.getElementById(k);
+  var dbRef = firebase.database().ref().child(path + g + "/" + k);
+
+  dbRef.on("value", snap => (carico.value = snap.val()));
+}
+
+caricaElementoLista("livello" ,"livello0");
+caricaElementoLista("livello" ,"livello1");
+caricaElementoLista("livello" ,"livello2");
+caricaElementoLista("livello" ,"livello3");
+caricaElementoLista("livello" ,"livello4");
+caricaElementoLista("livello" ,"livello5");
+caricaElementoLista("livello" ,"livello6");
+caricaElementoLista("livello" ,"livello7");
+caricaElementoLista("livello" ,"livello8");
+caricaElementoLista("livello" ,"livello9");
+
+function submitFocusLista(g , v) {
+  $("#" + v).focusout(function() {
+    submitListClick(v, g);
+  });
+}
+
+submitFocusLista("livello" ,"livello0");
+submitFocusLista("livello" ,"livello1");
+submitFocusLista("livello" ,"livello2");
+submitFocusLista("livello" ,"livello3");
+submitFocusLista("livello" ,"livello4");
+submitFocusLista("livello" ,"livello5");
+submitFocusLista("livello" ,"livello6");
+submitFocusLista("livello" ,"livello7");
+submitFocusLista("livello" ,"livello8");
+submitFocusLista("livello" ,"livello9");
